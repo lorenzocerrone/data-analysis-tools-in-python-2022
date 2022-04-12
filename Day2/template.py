8f196637ef4e0499f020664b761d71125f52e3da
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2022-04-12 09:45:14 +0200

    add template.py

---
+++ Day2/template.py
@@ -0,0 +1,29 @@
+"""
+
+a lot of python code
+
+"""
+import numpy as np
+
+
+def something_you_need_to_implement(a, b, c):
+    pass
+    return a + b + c
+    
+    
+"""
+
+ a lot more python
+ 
+ 
+"""
+
+if __name__ == '__main__':
+    c = 1
+    ...
+    c = str(c)
+    ...
+    a, b = 1, 1
+
+    result = something_you_need_to_implement(a, b, c)
+    print(result)
